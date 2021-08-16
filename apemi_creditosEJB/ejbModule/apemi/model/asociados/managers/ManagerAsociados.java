c583481f4f9c5df328ee7eeca88fe8a723a12440
Carlos Lopez <calopezs1110@gmail.com>
2021-08-16 17:56:56 -0500

    Arreglo de asignacion de modulos

--- apemi_creditosEJB/ejbModule/apemi/model/asociados/managers/ManagerAsociados.java
+++ apemi_creditosEJB/ejbModule/apemi/model/asociados/managers/ManagerAsociados.java
@@ -6,9 +6,11 @@ import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import apemi.model.auditoria.managers.ManagerAuditoria;
 import apemi.model.core.entities.AsoCiudad;
 import apemi.model.core.entities.SegUsuario;
 import apemi.model.core.managers.ManagerDAO;
+import apemi.model.seguridades.dtos.LoginDTO;
 
 /**
  * Session Bean implementation class ManagerAsociados
@@ -18,10 +20,30 @@ import apemi.model.core.managers.ManagerDAO;
 public class ManagerAsociados {
 	@EJB
 	private ManagerDAO mDAO;
+	@EJB
+	private ManagerAuditoria mAuditoria;
     
     public ManagerAsociados() {
        
     }
+    
+    public void insertarCiudad(AsoCiudad asoCiudad) throws Exception {
+    	mDAO.insertar(asoCiudad);
+    }
+    
+    public void actualizarCiudad(LoginDTO loginDTO ,AsoCiudad edicionCiudad) throws Exception {
+    	AsoCiudad ciudad = (AsoCiudad) mDAO.findById(AsoCiudad.class, edicionCiudad.getIdCiudad());
+    	ciudad.setNombre(edicionCiudad.getNombre());
+    	mDAO.actualizar(ciudad);
+    	mAuditoria.mostrarLog(loginDTO, getClass(), "Actualizar ciudad", "Se actualizó la ciudad"+ciudad.getNombre());
+    }
+    
+    
+    public void eliminarCiudad(int idAsoCiudad)throws Exception  {
+    	AsoCiudad ciudad = (AsoCiudad) mDAO.findById(AsoCiudad.class, idAsoCiudad);
+    	mDAO.eliminar(AsoCiudad.class, ciudad.getIdCiudad());
+    }
+    
     
     public List<AsoCiudad> findAllCiudades(){
     	return mDAO.findAll(AsoCiudad.class, "nombre");
---
+++ apemi_creditosWeb/src/main/java/apemi/controller/asociados/BeanAsoCiudades.java
@@ -0,0 +1,45 @@
+package apemi.controller.asociados;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import apemi.controller.JSFUtil;
+import apemi.model.asociados.managers.ManagerAsociados;
+import apemi.model.core.entities.AsoCiudad;
+
+@Named
+@SessionScoped
+public class BeanAsoCiudades implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@EJB
+	private ManagerAsociados managerAsociados;
+	
+	private AsoCiudad nuevaCiudad;
+	private List<AsoCiudad> listaCiudades;
+
+	public BeanAsoCiudades() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String actionMenuCiudades() {
+		listaCiudades = managerAsociados.findAllCiudades();
+		return "ciudades";
+	}
+	
+	public void actionListenerInsertarNuevaCiudad() {
+		try {
+			managerAsociados.insertarCiudad(nuevaCiudad);
+			JSFUtil.crearMensajeINFO("Ciudad insertada correctamente");
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	
+
+}
